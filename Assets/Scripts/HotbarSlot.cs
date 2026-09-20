3b53d310f2626daa6a574bbad46e93747ba38333
dylanemanning <dylanemanning@gmail.com>
2026-09-20 15:07:41 -0400

    Trying to merge hotbar integration

---
+++ Assets/Scripts/HotbarSlot.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HotbarSlot : MonoBehaviour
+{
+    [SerializeField] private Image background;
+    [SerializeField] private Image blockIcon;
+
+    [SerializeField] private Color normalColor = Color.white;
+    [SerializeField] private Color selectedColor = Color.yellow;
+
+    public void SetSelected(bool selected)
+    {
+        if (selected)
+        {
+            background.color = selectedColor;
+        }
+        else
+        {
+            background.color = normalColor;
+        }
+    }
+
+    public void SetIcon(Sprite icon)
+    {
+        blockIcon.sprite = icon;
+    }
+}
